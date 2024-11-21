76f4e8017b160a5541804de2e46f1d20ebf3c33c
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2024-11-21 15:21:47 +0100

    Add new mesh generation code

--- mARble-run/Assets/Scripts/Draw_with_meshes.cs
+++ mARble-run/Assets/Scripts/Draw_with_meshes.cs
@@ -4,18 +4,19 @@ using System.Collections.Generic;
 using UnityEngine.Splines;
 using Unity.Mathematics;
 
-public class Draw_with_meshes : MonoBehaviour
+public class VRPathDrawer : MonoBehaviour
 {
     public XRNode controllerNode = XRNode.RightHand; // Choose which controller to use
     public float minDistance = 0.01f; // Minimum distance between points and construction points
     public float maxDistToConnect = 0.02f;
-    public GameObject rampPrefab; // Prefab for the line renderer
-    
+    //public GameObject rampPrefab; // Prefab for the line renderer
+
     private Spline spline;
     private Vector3 lastPoint;
     private bool isDrawing = false;
     //private List<GameObject> segments = new List<GameObject>();
     private GameObject segment;
+
     private Vector3 currStartPoint;
     private Vector3 currEndPoint;
 
@@ -24,6 +25,7 @@ public class Draw_with_meshes : MonoBehaviour
 
     private GameObject meshObject;
     private Mesh mesh;
+    public Material mat;
 
     //start and end point stores for connecting pieces
     private List<int> spline_startpoint_indices = new List<int>();
@@ -37,7 +39,7 @@ public class Draw_with_meshes : MonoBehaviour
     private List<int> tris = new List<int>();
 
     //number of vertices around each single point (dont change atm)
-    private const int num_vertices_per_point = 4;
+    private int numVertsPerPoint = 4;
     //distance of vertices generated from the spline
     private float width = 1.0f;
 
@@ -102,7 +104,10 @@ public class Draw_with_meshes : MonoBehaviour
         spline_endpoint_points.Add(currEndPoint);
         //Add Connecting Faces
         ConnectEndpoints();
-        
+
+        mesh.SetVertices(vertices.ToArray());
+        mesh.SetTriangles(tris.ToArray(), 0);
+
     }
 
 
@@ -115,7 +120,7 @@ public class Draw_with_meshes : MonoBehaviour
 
 
         //We take the endpoints of the newly generated spline
-        Vector3 new_startpoint = spline_startpoint_points[spline_startpoint_points.Count-1];
+        Vector3 new_startpoint = spline_startpoint_points[spline_startpoint_points.Count - 1];
         int new_startpoint_index = spline_startpoint_indices[spline_startpoint_indices.Count - 1];
         Vector3 new_endpoint = spline_endpoint_points[spline_endpoint_points.Count - 1];
         int new_endpoint_index = spline_endpoint_indices[spline_endpoint_indices.Count - 1];
@@ -126,7 +131,7 @@ public class Draw_with_meshes : MonoBehaviour
         //find another startpoint to connect new endpoint to (always start->end)
         float dist = float.MaxValue;
 
-        for(int i = 0; i < spline_startpoint_points.Count-1; i++)
+        for (int i = 0; i < spline_startpoint_points.Count - 1; i++)
         {
             if (Vector3.Distance(new_endpoint, spline_startpoint_points[i]) <= maxDistToConnect)
             {
@@ -135,7 +140,7 @@ public class Draw_with_meshes : MonoBehaviour
                     start_connection_index = spline_startpoint_indices[i];
                 }
             }
-                
+
 
         }
 
@@ -154,80 +159,94 @@ public class Draw_with_meshes : MonoBehaviour
         }
 
         //make the additional faces
-        if(start_connection_index >= 0)
+        if (start_connection_index >= 0)
         {
-            FillFaces_single_step(start_connection_index, new_endpoint_index);
+            FillFaces_single_step(new_endpoint_index, start_connection_index);
         }
         if (end_connection_index >= 0)
         {
-            FillFaces_single_step(new_startpoint_index, end_connection_index);
+            FillFaces_single_step(end_connection_index, new_startpoint_index);
         }
 
-           
     }
 
 
-    
+
 
     void ConstructRoute()
     {
-        float percentage = minDistance / spline.GetLength();
+        //float percentage = minDistance / spline.GetLength();
+        float percentage = 0.1f;
         int startpoint_index = vertices.Count;
         int new_point_counter = 0;
         for (float t = 0f; t <= 1; t += percentage)
         {
-            new_point_counter++;
-            if(SplineUtility.Evaluate(spline, t, out float3 position, out float3 tangent, out float3 upVector))
-            {   
+
+            if (SplineUtility.Evaluate(spline, t, out float3 position, out float3 tangent, out float3 upVector))
+            {
+                new_point_counter++;
                 //Instantiate(rampPrefab, position, Quaternion.LookRotation(tangent), segment.transform);
 
 
                 //atm Generate 4 vertices around the points in shape in a V shape
-              
+
+
                 Vector3 right = Vector3.Cross(tangent, upVector).normalized;
 
                 vertices.Add(((Vector3)position) + (right * width));
-                vertices.Add(((Vector3)position) + (-right * width));
                 vertices.Add(((Vector3)position) + (-((Vector3)upVector) * width));
+                vertices.Add(((Vector3)position) + (-right * width));
                 vertices.Add(((Vector3)position) + (-((Vector3)upVector) * width * 1.5f));
-
             }
         }
         //save the start/endpoints for connection later
         spline_startpoint_indices.Add(startpoint_index);
-        spline_endpoint_indices.Add(startpoint_index + (num_vertices_per_point * new_point_counter));
+        spline_endpoint_indices.Add(startpoint_index + (numVertsPerPoint * (new_point_counter - 1)));
+        FillFaces_along_spline(startpoint_index, startpoint_index + (numVertsPerPoint * (new_point_counter - 1)));
+        for (int i = 0; i < spline_startpoint_indices.Count; i++)
+        {
+            print(spline_startpoint_indices[i]);
+        }
+        for (int i = 0; i < spline_endpoint_indices.Count; i++)
+        {
+            print(spline_endpoint_indices[i]);
+        }
     }
 
 
     //go around a spline and connect subsequent points)
     void FillFaces_along_spline(int startpoint_offset, int endpoint_offset)
     {
-        for(int i = startpoint_offset; i < endpoint_offset; i += num_vertices_per_point)
+        for (int i = startpoint_offset; i < endpoint_offset; i += numVertsPerPoint)
         {
-            FillFaces_single_step(i, i + 1);
+            FillFaces_single_step(i, i + numVertsPerPoint);
         }
 
     }
-    
+
     //connect vertices of 2 points
     void FillFaces_single_step(int offset1, int offset2)
     {
-        for (int i = 0; i < num_vertices_per_point - 1; i++)
+        for (int i = 0; i < numVertsPerPoint - 1; i++)
         {
             tris.Add(offset1 + i);
             tris.Add(offset2 + i);
             tris.Add(offset1 + i + 1);
 
+
             tris.Add(offset1 + i + 1);
-            tris.Add(offset2 + i + 1);
             tris.Add(offset2 + i);
+            tris.Add(offset2 + i + 1);
         }
-        tris.Add(offset1 + num_vertices_per_point - 1);
-        tris.Add(offset2 + num_vertices_per_point - 1);
+
+        tris.Add(offset1 + numVertsPerPoint - 1);
+        tris.Add(offset2 + numVertsPerPoint - 1);
         tris.Add(offset1);
 
-        tris.Add(offset1);
         tris.Add(offset2);
-        tris.Add(offset2 + num_vertices_per_point - 1);
+        tris.Add(offset1);
+        tris.Add(offset2 + numVertsPerPoint - 1);
+
+
     }
 }
