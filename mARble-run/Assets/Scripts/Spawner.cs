94fd8ccaf096a4b4c7c9d40fd73574d806cbbe97
Edoardo Negri <enegri@ethz.ch>
2024-11-25 12:56:26 +0100

    changed the grabbables

--- mARble-run/Assets/Scripts/Spawner.cs
+++ mARble-run/Assets/Scripts/Spawner.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Spawner : MonoBehaviour
 {
-    public GameObject spawnObject;
+    public GameObject spawnObject; // The object to instantiate
 
     public void SpawnObject()
     {
-        Instantiate(spawnObject, transform.position, transform.rotation);
+
+        Instantiate(spawnObject, transform.position + (transform.forward * 0.2f), transform.rotation);
     }
 }
