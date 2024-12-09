a19139bdcc1e90b037ee07c71db6bb345d1a9bc2
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2024-12-09 12:59:05 +0100

    Adjustments to sizes and scripts

--- mARble-run/Assets/Scripts/FollowCamera.cs
+++ mARble-run/Assets/Scripts/FollowCamera.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset;
+    public float xOffset;
+    public float yOffsetMultiplier;
+    public float zOffset;
+    public Vector3 rotation;
 
-
+    void Start()
+    {
+        Vector3 offset = new Vector3(xOffset, 0, zOffset);
+        offset = target.TransformDirection(offset);
+        xOffset = offset.x;
+        zOffset = offset.z;
+        transform.rotation = Quaternion.Euler(rotation);
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.position + offset;
+        transform.position = new Vector3(
+            target.position.x + xOffset,
+            target.position.y * yOffsetMultiplier,
+            target.position.z + zOffset
+        );
     }
 }
